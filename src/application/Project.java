8376603794f3467157f2235c9d130f8402dcd35b
Lavinya Correia <lavinya.correia01@gmail.com>
2020-12-31 12:15:49 -0300

    Department entity class

---
+++ src/application/Project.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.entities.Department;
+
+public class Project {
+
+	public static void main(String[] args) {
+
+		Department obj = new Department(1, "BOOKS");
+		System.out.println(obj);
+	}
+
+}
