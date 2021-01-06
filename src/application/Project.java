60e67c5769a56e837e9d6038abb136392411b80e
Lavinya Correia <lavinya.correia01@gmail.com>
2021-01-06 16:15:59 -0300

    finById implementation

--- src/application/Project.java
+++ src/application/Project.java
@@ -17,9 +17,15 @@ public class Project {
 		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
-		System.out.println(" seller findByDepartment");
+		System.out.println("Seller findByDepartment");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+		
+		System.out.println("Seller findAll");
+		list = sellerDao.findAll();
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
