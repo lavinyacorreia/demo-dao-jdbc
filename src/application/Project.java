2c4d1ae93fd36be3d0bf1b2333fb6b6211db12f0
Lavinya Correia <lavinya.correia01@gmail.com>
2021-01-06 20:06:23 -0300

    delete implementation

--- src/application/Project.java
+++ src/application/Project.java
@@ -3,6 +3,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,6 +13,8 @@ import model.entities.Seller;
 public class Project {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 	
 		System.out.println("Seller findById");
 		SellerDao sellerDao = DaoFactory.createSellerDao();
@@ -37,6 +40,18 @@ public class Project {
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
-	}
+		
+		System.out.println("Seller update");
+		seller = sellerDao.findById(1);
+		seller.setName("Maria vai com as outras");
+		sellerDao.update(seller);
+		
+		System.out.println("Seller delete");
+		System.out.println("Enter id for delete: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed");
+		sc.close();
+		}
 
 }
